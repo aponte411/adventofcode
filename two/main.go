09d19918bbf61252e09a0a3af47a5abcb281d741
aponte411 <aponted511@gmail.com>
2020-12-17 15:57:55 -0500

    feat: finish part 2

--- two/main.go
+++ two/main.go
@@ -2,30 +2,34 @@ package main
 
 import (
 	"bufio"
-    "strconv"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func IsValid(min, max int, char string, str string) bool {
-    count := 0
-    for _, ch := range str {
-        if string(ch) == char {
-            count += 1
-        }
-    }
-    if count <= max && count >= min {
-        return true
-    } else {
-        return false
-    }
+	count := 0
+	for _, ch := range str {
+		if string(ch) == char {
+			count += 1
+		}
+	}
+	if count <= max && count >= min {
+		return true
+	} else {
+		return false
+	}
+}
+
+func IsValid2(min, max int, char string, password string) bool {
+	return (char == string(password[min])) != (char == string(password[max]))
 }
 
 func StringToInt(str string) int {
-    res, _ := strconv.Atoi(str)
-    return res
+	res, _ := strconv.Atoi(str)
+	return res
 }
 
 func main() {
@@ -40,21 +44,28 @@ func main() {
 	for scanner.Scan() {
 		inputs = append(inputs, scanner.Text())
 	}
-    numOfValidPasswords := 0
-    for _,line := range inputs {
-        // Get password and policy
-        pol_and_pass := strings.Split(line, ":")
-        policy, password := pol_and_pass[0], pol_and_pass[1]
-        // parse policy
-        res := strings.Split(policy, "-")
-        min, rest := StringToInt(res[0]), res[1]
-        tmp := strings.Split(rest, " ")
-        max, char := StringToInt(tmp[0]), tmp[1]
-        // Part 1, if valid count
-        if IsValid(min, max, char, password) {
-            numOfValidPasswords += 1
-        }
+	numOfValidPasswords1 := 0
+	numOfValidPasswords2 := 0
 
-    }
-    fmt.Printf("Final number of valid passwords: %v, Out of: %v\n", numOfValidPasswords, len(inputs))
+	for _, line := range inputs {
+		// Get password and policy
+		pol_and_pass := strings.Split(line, ":")
+		policy, password := pol_and_pass[0], pol_and_pass[1]
+		// parse policy
+		res := strings.Split(policy, "-")
+		min, rest := StringToInt(res[0]), res[1]
+		tmp := strings.Split(rest, " ")
+		max, char := StringToInt(tmp[0]), tmp[1]
+		// Part 1, if valid count
+		if IsValid(min, max, char, password) {
+			numOfValidPasswords1 += 1
+		}
+        // Part 2
+		if IsValid2(min, max, char, password) {
+			numOfValidPasswords2 += 1
+		}
+	}
+	fmt.Printf("Final number of valid passwords Part 1: %v, Out of: %v\n", numOfValidPasswords1, len(inputs))
+
+	fmt.Printf("Final number of valid passwords Part 2: %v, Out of: %v\n", numOfValidPasswords2, len(inputs))
 }
